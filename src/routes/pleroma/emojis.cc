b570356af33b59afdc934227e8cd795ba142788b
velzie <velzie@velzie.rip>
2024-12-13 16:49:55 -0500

    pleromapi: /api/v1/pleroma/emoji implementation

--- src/routes/pleroma/emojis.cc
+++ src/routes/pleroma/emojis.cc
@@ -1,8 +1,31 @@
+#include "database.h"
+#include "entities/Emoji.h"
 #include <router.h>
 #include <common.h>
 
 GET(pleroma_emojis, "/api/v1/pleroma/emoji") {
-  OK(json::object(), MIMEJSON);
+  json emojis = json::object();
+
+  auto q = STATEMENT("SELECT * FROM emoji");
+  while (q.executeStep()) {
+    Emoji e;
+    e.load(q);
+    emojis[e.shortcode] = {
+        {"tags", {FMT("pack:{}", e.host)}},
+        {"image_url", "/302?url="+e.imageurl}
+    };
+  }
+
+  OK(emojis, MIMEJSON);
+}
+
+// jank but whatever
+GET(redirect302, "/302") {
+  string url(req->getQuery("url"));
+
+  res->writeStatus("302");
+  res->writeHeader("Location", url);
+  res->endWithoutBody();
 }
 
 GET(pleroma_stickers, "/static/stickers.json") {
